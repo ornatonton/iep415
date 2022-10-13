77e70e466ff48f20cf18909300bb28ee1d2b8b3c
Orlando Ritchie Natonton <ornatonton@urios.edu.ph>
2022-10-14 07:46:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>IEP415 Exercises Website</title></head>
+<body>
+<h1>IEP415: Information Systems Online Exercises</h1><br>
+First Semester AY 2022-2023
+<br>
+Student Name: Orlando Ritchie R. Natonton
+<br>
+<h2>Exercise 1: Adding Two Values</h2>
+<a href="http://iep415.infinityfreeapp.com/add.php">Add two variables</a>
+<br>
+<h2>Exercise 2: MySQL Database Exercise Outputs</h1>
+<a href="http://iep415.infinityfreeapp.com/connect.php" target = "_blank">A. Connection to MySQL Database</a><br>
+<a href="http://iep415.infinityfreeapp.com/display.php" target = "_blank">B. Display my database data</a>
+
+</body>
+</html>
